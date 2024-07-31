98a11eac3f212612d1fdc24c4a1eb522f4bad1a8
Agnibha Chakraborty <agnichak@amazon.com>
2024-07-31 16:27:03 +0530

    Init commit

---
+++ src/components/AuthInputs.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button";
+import Input from "./Input";
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === "email") {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes("@");
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <div id="auth-inputs" className="w-full max-w-sm p-8 rounded shadow-md mx-auto bg-gradient-to-b from-stone-700 to to-stone-800">
+        <div className="flex flex-col gap-2">
+        <Input
+          label="Email"
+          invalid={emailNotValid}
+          type="email"
+          className={emailNotValid ? "invalid" : undefined}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+
+        
+          <Input
+            label="Password"
+            invalid={emailNotValid}
+            type="password"
+            className={passwordNotValid ? "invalid" : undefined}
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+          />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
+          Create a new account
+        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
+      </div>
+    </div>
+  );
+}
